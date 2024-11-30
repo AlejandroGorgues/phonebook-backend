4f217d2f45f6c5be29e9fd8060f63443deb9c2c9
Alejandro <email>
2024-11-30 22:11:16 +0100

    Added linter and readme

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # phonebook-backend
+This repository contains the code to run the full phonebook web application related to the phonebook project developed on the [fullstackopen](https://fullstackopen.com/en/) free course of the Helsinki University.
+
+More specifically, with the dist folder generated from the frontend and copied to this root folder, it can be deployed only one time.
+
+The functionality of the backend behaves the same as the ones declared in the frontend repository.
+
+## Extra
+* For testing purposes, this repository has been deployed on the free tier of the Render service.
+* The link to the frontend repository is [here](https://github.com/AlejandroGorgues/phonebook-frontend).
--- models/person.js
+++ models/person.js
@@ -10,16 +10,30 @@ console.log('connecting to', url)
 
 mongoose.connect(url)
 
-  .then(result => {
+  .then(() => {
     console.log('connected to MongoDB')
   })
   .catch(error => {
     console.log('error connecting to MongoDB:', error.message)
   })
 
-  const phonebookSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+const phonebookSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    required: [true, 'User phone number required'],
+    validate: {
+      validator: function(v) {
+        return /^\d{2,3}-\d+$/.test(v)
+      },
+      message: props => `${props.value} is not a valid phone number`
+    }
+  },
 })
 
 phonebookSchema.set('toJSON', {
